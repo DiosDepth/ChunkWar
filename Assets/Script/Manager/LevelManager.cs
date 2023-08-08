1124619b0c319d1ff2f87254359cdc735ee15883
PC-DIOSDEPTH\Dios Depth <84689343@qq.com>
2023-08-08 23:45:08 +0800

    Revert "增加一个刷新参数， RectAISpawnSetting"
    This reverts commit e2454ec4589771cf4ad80a9182b5899814dcef59.

--- Assets/Script/Manager/LevelManager.cs
+++ Assets/Script/Manager/LevelManager.cs
@@ -110,7 +110,7 @@ public class LevelManager : Singleton<LevelManager>,EventListener<LevelEvent>, E
             AIFactory aIFactory = obj.GetComponent<AIFactory>();
             aIFactory.PoolableSetActive(true);
             aIFactory.Initialization();
-            aIFactory.StartSpawn(spawnpoint,new RectAISpawnSetting());
+            aIFactory.StartSpawn(spawnpoint, null);
         });
     }
 
